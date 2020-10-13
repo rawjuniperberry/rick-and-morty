0ca1e525b726b124b7058e394f27a2016333be5d
rawjuniperberry <rawjuniperberry@gmail.com>
2020-10-14 01:28:11 +0200

    Setup

---
+++ src/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Home.module.scss'
+
+export function Home() {
+    return (
+        <article className={styles.home}>
+            <h1>Rick and Morty encyclopedia</h1>
+        </article>
+    )
+}
+
+
